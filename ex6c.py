5b62e014b09349f2c20a85b4caf9b287190db543
Fábio Silva <a50536@alunos.isel.pt>
2024-04-19 18:49:27 +0100

    Minor changes.

--- ex6c.py
+++ ex6c.py
@@ -7,7 +7,7 @@ def berComparionWithFile(file, p):
     f.close()
     binary_str = ""
     for c in text:
-        binary_str += format(ord(c), 'b')
+        binary_str += format(ord(c), '08b')
     newSeq = bsc(binary_str, p)
     errors = countErrors(binary_str, newSeq)
     ber = errors / len(binary_str)
@@ -15,10 +15,22 @@ def berComparionWithFile(file, p):
     print("Number of errors: ", errors)
     print("Original BER: ", p)
     print("New BER: ", ber)
+    return newSeq
+
+def binaryStringToFile(binary_str):
+    f = open("resources/binary_str.txt", "w")
+    for i in range(0, len(binary_str), 8):
+        char = chr(int(binary_str[i:i+8],2))
+        f.write(char)
+    f.close()
+
+
+
 
 
 def main(file):
-    berComparionWithFile(file, 0.1)
+    newSeq = berComparionWithFile(file, 0.01)
+    binaryStringToFile(newSeq)
     
 if __name__ == "__main__":
     main("resources/alice29.txt")
