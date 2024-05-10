73aacbe3856ad1aef801d1bac7ce7de744ce80ae
Bruno Raposo A50553 <A50553@alunos.isel.pt>
2024-05-10 16:57:50 +0100

    Almost finished exercise 2b

--- Module_2/ex2b.py
+++ Module_2/ex2b.py
@@ -1,5 +1,59 @@
-from
+import random
 
-def calculateRedundantBits(k):
-    calculator = 
-    b = 
+from crccheck.crc import *
+from crccheck.checksum import *
+from ex2a import burstBSC
+
+crcBits = 16
+
+def generateBinarySeq(k):
+    seq = ""
+    for i in range(k):
+        seq += str(random.randint(0, 1))
+    return seq
+
+def seqToByteArray(seq):
+    data = bytearray()
+    for b in range(0, len(seq), 8):
+        byte = int(seq[b:b+8], 2)
+        data.append(byte)
+    return data
+
+def parityBits(data):
+    parBits = format(Crc16.calc(data), '08b')
+    if(len(parBits) < crcBits):
+        newBits = ""
+        for i in range(0, crcBits - len(parBits)):
+            newBits += '0'
+        newBits += parBits
+        return newBits
+    return parBits    
+
+def encodeMsg(seq):
+    data = seqToByteArray(seq)
+    b = parityBits(data)
+    seq += b
+    return seq
+
+def decodeMsg(result, k):
+    m = result[:crcBits]
+    mByte = seqToByteArray(m)
+    expectedParBits = result[k:]
+    actualParBits = parityBits(mByte)
+    print(expectedParBits)
+    print(actualParBits)
+    if(expectedParBits != actualParBits):
+        print("Error Detected")
+
+
+def examples():
+    print("AH")
+
+def main():
+    seq = generateBinarySeq(1024)
+    encode = encodeMsg(seq)
+    #result = burstBSC(encode, 5, 0.000000001)
+    decode = decodeMsg(encode, 1024)
+
+if __name__ == "__main__":
+    main()
